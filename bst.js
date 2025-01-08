156e94e2010635c226c7d65c5f6a516f5094d875
kevnhech <kevnhech@gmail.com>
2025-01-08 14:11:29 -0800

    Remove duplicates and sort array

--- bst.js
+++ bst.js
@@ -8,11 +8,12 @@ class Node {
 
 class Tree {
   constructor(array) {
-    this.array = array;
+    this.array = array.sort((a, b) => a - b);
+    array = [...new Set(array)];
     this.root = this.buildTree(array);
   }
-
   buildTree(array, start = 0, end = array.length - 1) {
+    console.log(array);
     if (start > end) return null;
     let mid = Math.floor((start + end) / 2);
     let root = new Node(array[mid]);
@@ -21,3 +22,21 @@ class Tree {
     return root;
   }
 }
+
+let bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+let node = bst.root;
+
+ const prettyPrint = (node, prefix = "", isLeft = true) => {
+   if (node === null) {
+     return;
+   }
+   if (node.right !== null) {
+     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+   }
+   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+   if (node.left !== null) {
+     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+   }
+ };
+
+prettyPrint(node);
