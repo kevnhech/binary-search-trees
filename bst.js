4d83ad430c0797b50369b8c10c8a5d95d3db9cc2
kevnhech <kevnhech@gmail.com>
2025-01-08 13:46:28 -0800

    Build buildTree() function

--- bst.js
+++ bst.js
@@ -9,6 +9,15 @@ class Node {
 class Tree {
   constructor(array) {
     this.array = array;
-    this.root = this.buildTree();
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+    let mid = Math.floor((start + end) / 2);
+    let root = new Node(array[mid]);
+    root.left = this.buildTree(array, start, mid - 1);
+    root.right = this.buildTree(array, mid + 1, end);
+    return root;
   }
 }
